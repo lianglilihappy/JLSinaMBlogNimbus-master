afdc8fbfe69b129db3d93bbc491e24a2d579b674
lianglilihappy <lianglilihappy@163.com>
2018-03-09 10:59:57 +0800

    更改注释
    测试更改注释

---
+++ IosProgramexercise/ViewController.m
@@ -0,0 +1,47 @@
+//
+//  ViewController.m
+//  IosProgramexercise
+//
+//  Created by 万旭杰 on 15/8/15.
+//  Copyright (c) 2015年 万旭杰. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "XJAlbum.h"
+#define screenWidth [UIScreen mainScreen].bounds.size.width
+#define screenHeight [UIScreen mainScreen].bounds.size.height
+@interface ViewController ()
+
+@end
+
+
+@implementation ViewController
+{
+    XJAlbum * album;
+    UIImageView * imgview;
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    imgview = [[UIImageView alloc] initWithFrame:CGRectMake(0, screenHeight/3, screenWidth, screenHeight/3)];
+    [self.view addSubview:imgview];
+    imgview.backgroundColor = [UIColor orangeColor];
+    // 封装一个对 imageview 的方法
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(album)];
+    imgview.userInteractionEnabled = YES;
+    [imgview addGestureRecognizer:tap];
+    NSTimeInterval  timeInterval = 0.01;
+    [self performSelector:@selector(album) withObject:nil afterDelay:timeInterval];
+}
+
+-(void)album
+{
+    NSArray *netarr = @[
+                        @"http://www.xinhuanet.com/politics/2018lh/2018-03/05/129822998_15202180711941n.jpg",@"http://www.xinhuanet.com/politics/2018lh/2018-03/05/129822998_15202180711941n.jpg"];
+    album = [[XJAlbum alloc]initWithImgUrlArr:netarr CurPage:0];
+    album.photoFrame = imgview.frame;
+    [self.navigationController presentViewController:album animated:YES completion:nil];
+}
+
+@end
